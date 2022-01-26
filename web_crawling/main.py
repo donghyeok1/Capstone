4a91a387b8ad84965f534f727dd9c118431683ad
donghyeok choi <07385@naver.com>
2022-01-26 21:46:40 +0900

    modifying advance func

--- web_crawling/main.py
+++ web_crawling/main.py
@@ -1,155 +1,235 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QLineEdit, QLabel, QPushButton, QGridLayout, QHBoxLayout, QVBoxLayout
-from PyQt5.QtCore import Qt, QUrl, QEvent
-from PyQt5.QtGui import QIcon
+from tkinter import *
+from tkinter import filedialog
+import PIL.Image, PIL.ImageTk
+from ffpyplayer.player import MediaPlayer # pip install ffpyplayer
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-import urllib.request
-import time
 from IPython.display import display
 from webdriver_manager.chrome import ChromeDriverManager
 import warnings
 warnings.filterwarnings(action='ignore')
-from PyQt5.QtWebEngineWidgets import QWebEngineSettings, QWebEngineView
+import cv2
+import pytube
+from moviepy.editor import *
+import time
 
-__version__ = 'capston'
-__author__ = '624È£'
+class videoGUI:
+    def __init__(self, window, window_title):
+        self.window = window
+        self.window.title(window_title)
+        self.url = self.get_video()
+        self.crawling(self.url)
+        top_frame = Frame(self.window)
+        top_frame.pack(side=TOP, pady=5)
+        bottom_frame = Frame(self.window)
+        bottom_frame.pack(side=BOTTOM, pady=5)
+        self.cap = None
+        self.pause = False   # Parameter that controls pause button
+        self.delay = 15   # ms
+        self.canvas = Canvas(top_frame)
+        self.canvas.pack()
+        self.open_file()
+
+        self.btn_play = Button(bottom_frame, text="Advance", width=15, command=self.advance_video)
+        self.btn_play.grid(row=0, column=0)
+
+        # Play Button
+        self.btn_play=Button(bottom_frame, text="Play", width=15, command=self.play_video)
+        self.btn_play.grid(row=0, column=1)
+
+        # Pause Button
+        self.btn_pause=Button(bottom_frame, text="Pause", width=15, command=self.pause_video)
+        self.btn_pause.grid(row=0, column=2)
+        self.window.mainloop()
+
+    def open_file(self):
+        self.pause = True
+        self.elaspedTime=0
+        self.totalElaspedTime=0
+        self.filename = 'C:\youtube\\test.mp4'
+        #audio, sr = librosa.load(self.filename+".wav")
+        #librosa.display.waveplot(audio, sr=sr)
+        #plt.title('draw waveform')
+        #plt.show()
+
+        # Open the video file
+        self.cap = cv2.VideoCapture(self.filename)
+        self.width = self.cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+        self.height = self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        self.canvas.config(width = self.width, height = self.height)
+        self.fps = self.cap.get(cv2.CAP_PROP_FPS)
+        self.total_frame = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        self.total_sec = self.total_frame / self.fps
+        self.test = 10 / self.total_sec
+        ff_opts = {'paused':True, 'vn':True}  # only audio
+        self.aux = MediaPlayer(self.filename, ff_opts=ff_opts, callback= self.audioCallback)
+        pass
+
+    def get_videoFrame(self):   # get only one frame
+        try:
+            if self.cap.isOpened():
+                self.elapsedTime=  time.time()- self.start_time #sec
+                seekVal= int( self.fps*(self.totalElaspedTime+self.elapsedTime))
+                self.cap.set(cv2.CAP_PROP_POS_FRAMES, seekVal)
+                ret, frame = self.cap.read()
+                if frame is None:
+                    return (False, None)
+                return (ret, cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+        except Exception as e:
+            print(e)
+            return (False, None)
+
+    def play_video(self):
+        print("play_video...")
+        print('Position:', int(self.cap.get(cv2.CAP_PROP_POS_FRAMES)))
+        if self.pause:
+            self.pause= False
+            self.start_time = time.time()
+            self.play_loop()
+        pass
+
+    def pause_video(self):
+        print("pause_video...")
+        if self.pause:
+            return
+        self.pause= True
+        self.aux.set_pause(self.pause)  # Stop the audio
+        self.totalElaspedTime = self.totalElaspedTime + self.elapsedTime
+
+    def advance_video(self):
+        print("advance_video...")
+
+        self.totalElaspedTime = self.totalElaspedTime + self.elapsedTime + 10
+        self.aux.seek(self.test * self.aux.get_metadata()['duration'], relative=False)
+        self.play_video()
+
+        pass
 
 
-class YouTubePlayer(QWidget):
-    def __init__(self, video_id, parent=None):
-        super().__init__()
-        self.parent = parent
-        self.video_id = video_id
+    def play_loop(self):
+        if self.pause:
+            return
+        self.aux.set_pause(self.pause)  # Start/Stop the audio
+        audio_frame, val = self.aux.get_frame()
+        ret, video_frame = self.get_videoFrame()
+        if ret:
+            self.photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(video_frame))
+            self.canvas.create_image(0, 0, image = self.photo, anchor = NW)
+        else:
+            return
+        self.after_id = self.window.after(self.delay, self.play_loop)
+        pass
 
-        defaultSettings = QWebEngineSettings.globalSettings()
-        defaultSettings.setFontSize(QWebEngineSettings.MinimumFontSize, 25)
+    def crawling(self, url):
+        yt = pytube.YouTube(url)
 
-        self.layout = QVBoxLayout()
-        self.setLayout(self.layout)
-        self.input = QLineEdit()
-        self.input.installEventFilter(self)
-        self.input.setText(self.video_id)
-        self.addWebView(self.input.text())
+        videos = yt.streams.all()
 
-    def eventFilter(self, source, event):
-         if event.type() == QEvent.KeyPress:
-                 self.updateVideo()
-         return super().eventFilter(source, event)
+        yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download(
+            'C:\youtube', 'test.mp4')
 
-    def addWebView(self, video_id):
-        self.webview = QWebEngineView()
-        self.webview.setUrl(QUrl(f'https://www.youtube.com/embed/{self.video_id}?rel=0'))
-        self.layout.addWidget(self.webview)
+    def get_video(self):
+        feature = input('ê²€ìƒ‰ì–´ë¥¼ ì…ë ¥í•˜ì‹œì˜¤ : ')
 
+        driver = webdriver.Chrome(ChromeDriverManager().install())
+        driver.get('https://www.youtube.com')
 
-class YouTubeWindow(QWidget):
-    def __init__(self, url):
-        super().__init__()
-        self.setWindowTitle('YouTube Video Player')
-        self.setWindowIcon(QIcon('yt.png'))
-        self.setMinimumSize(900, 500)
-        self.players = []
+        n = 3
+        while n > 0:
+            print('ì›¹í˜ì´ì§€ë¥¼ ë¶ˆëŸ¬ì˜¤ëŠ” ì¤‘ì…ë‹ˆë‹¤..' + '..' * n)
+            time.sleep(1)
+            n -= 1
 
-        self.layout = QVBoxLayout()
-        self.setLayout(self.layout)
+        src = driver.find_element_by_name("search_query")
+        src.send_keys(feature)
+        src.send_keys(Keys.RETURN)
 
-        self.videoGrid = QGridLayout()
-        self.layout.addLayout(self.videoGrid)
+        n = 2
+        while n > 0:
+            print('ê²€ìƒ‰ ê²°ê³¼ë¥¼ ë¶ˆëŸ¬ì˜¤ëŠ” ì¤‘ì…ë‹ˆë‹¤..' + '..' * n)
+            time.sleep(1)
+            n -= 1
 
-        self.player = YouTubePlayer(url, parent=self)
-        self.videoGrid.addWidget(self.player, 0, 0)
+        print('ë°ì´í„° ìˆ˜ì§‘ ì¤‘ì…ë‹ˆë‹¤....')
 
-        self.layout.addWidget(QLabel(__version__ + ' by ' + __author__), alignment=Qt.AlignBottom | Qt.AlignRight)
+        html = driver.page_source
+        soup = BeautifulSoup(html)
 
-def get_video():
-    feature = input('°Ë»ö¾î¸¦ ÀÔ·ÂÇÏ½Ã¿À : ')
+        df_title = []
+        df_link = []
+        df_writer = []
+        df_view = []
+        df_date = []
 
-    driver = webdriver.Chrome(ChromeDriverManager().install())
-    driver.get('https://www.youtube.com')
+        for i in range(len(soup.find_all('ytd-video-meta-block', 'style-scope ytd-video-renderer byline-separated'))):
+            title = soup.find_all('a', {'id': 'video-title'})[i].text.replace('\n', '')
+            link = 'https://www.youtube.com/' + soup.find_all('a', {'id': 'video-title'})[i]['href']
+            writer = \
+                soup.find_all('ytd-channel-name', 'long-byline style-scope ytd-video-renderer')[i].text.replace('\n',
+                                                                                                                '').split(
+                    ' ')[0]
+            view = \
+                soup.find_all('ytd-video-meta-block', 'style-scope ytd-video-renderer byline-separated')[i].text.split(
+                    'â€¢')[
+                    1].split('\n')[3]
+            date = \
+                soup.find_all('ytd-video-meta-block', 'style-scope ytd-video-renderer byline-separated')[i].text.split(
+                    'â€¢')[
+                    1].split('\n')[4]
 
-    n = 3
-    while n > 0:
-        print('À¥ÆäÀÌÁö¸¦ ºÒ·¯¿À´Â ÁßÀÔ´Ï´Ù..' + '..' * n)
-        time.sleep(1)
-        n -= 1
+            df_title.append(title)
+            df_link.append(link)
+            df_writer.append(writer)
+            df_view.append(view)
+            df_date.append(date)
 
-    src = driver.find_element_by_name("search_query")
-    src.send_keys(feature)
-    src.send_keys(Keys.RETURN)
+        df_just_video = pd.DataFrame(columns=['ì˜ìƒì œëª©', 'ì±„ë„ëª…', 'ì˜ìƒurl', 'ì¡°íšŒìˆ˜', 'ì˜ìƒë“±ë¡ë‚ ì§œ'])
 
-    n = 2
-    while n > 0:
-        print('°Ë»ö °á°ú¸¦ ºÒ·¯¿À´Â ÁßÀÔ´Ï´Ù..' + '..' * n)
-        time.sleep(1)
-        n -= 1
+        df_just_video['ì˜ìƒì œëª©'] = df_title
+        df_just_video['ì±„ë„ëª…'] = df_writer
+        df_just_video['ì˜ìƒurl'] = df_link
+        df_just_video['ì¡°íšŒìˆ˜'] = df_view
+        df_just_video['ì˜ìƒë“±ë¡ë‚ ì§œ'] = df_date
 
-    print('µ¥ÀÌÅÍ ¼öÁı ÁßÀÔ´Ï´Ù....')
+        df_just_video.to_csv('C:\\youtube\\df_just_video.csv', encoding='utf-8-sig', index=False)
 
-    html = driver.page_source
-    soup = BeautifulSoup(html)
+        driver.close()
 
-    df_title = []
-    df_link = []
-    df_writer = []
-    df_view = []
-    df_date = []
+        result = input('ë°ì´í„°í”„ë ˆì„ ì €ì¥ì´ ì™„ë£Œë˜ì—ˆìŠµë‹ˆë‹¤! ë°ì´í„°í”„ë ˆì„ì„ ì¡°íšŒí•˜ì‹œê² ìŠµë‹ˆê¹Œ? (y/n)')
 
-    for i in range(len(soup.find_all('ytd-video-meta-block', 'style-scope ytd-video-renderer byline-separated'))):
-        title = soup.find_all('a', {'id': 'video-title'})[i].text.replace('\n', '')
-        link = 'https://www.youtube.com/' + soup.find_all('a', {'id': 'video-title'})[i]['href']
-        writer = \
-        soup.find_all('ytd-channel-name', 'long-byline style-scope ytd-video-renderer')[i].text.replace('\n', '').split(
-            ' ')[0]
-        view = \
-        soup.find_all('ytd-video-meta-block', 'style-scope ytd-video-renderer byline-separated')[i].text.split('?')[
-            1].split('\n')[3]
-        date = \
-        soup.find_all('ytd-video-meta-block', 'style-scope ytd-video-renderer byline-separated')[i].text.split('?')[
-            1].split('\n')[4]
+        if result == 'y':
+            display(df_just_video)
+            question = input('ì›í•˜ëŠ” ì˜ìƒì„ ì¬ìƒí•˜ì‹œê² ìŠµë‹ˆê¹Œ? (y/n)')
+            if question == 'y':
+                button = int(input('ì¬ìƒí•˜ê³ ì í•˜ëŠ” ì˜ìƒì˜ ë²ˆí˜¸(ì¶œë ¥ëœ í‘œ ê°€ì¥ ì™¼ìª½ì˜ ë²ˆí˜¸)ë¥¼ ì…ë ¥í•´ì£¼ì„¸ìš”.'))
+                url = df_just_video['ì˜ìƒurl'][button]
+                return url
 
-        df_title.append(title)
-        df_link.append(link)
-        df_writer.append(writer)
-        df_view.append(view)
-        df_date.append(date)
-
-    df_just_video = pd.DataFrame(columns=['¿µ»óÁ¦¸ñ', 'Ã¤³Î¸í', '¿µ»óurl', 'Á¶È¸¼ö', '¿µ»óµî·Ï³¯Â¥'])
-
-    df_just_video['¿µ»óÁ¦¸ñ'] = df_title
-    df_just_video['Ã¤³Î¸í'] = df_writer
-    df_just_video['¿µ»óurl'] = df_link
-    df_just_video['Á¶È¸¼ö'] = df_view
-    df_just_video['¿µ»óµî·Ï³¯Â¥'] = df_date
-
-    df_just_video.to_csv('C:\\Users\\c\\df_just_video.csv', encoding='utf-8-sig', index=False)
-
-    driver.close()
-
-    result = input('µ¥ÀÌÅÍÇÁ·¹ÀÓ ÀúÀåÀÌ ¿Ï·áµÇ¾ú½À´Ï´Ù! µ¥ÀÌÅÍÇÁ·¹ÀÓÀ» Á¶È¸ÇÏ½Ã°Ú½À´Ï±î? (y/n)')
-    if result == 'y':
-        display(df_just_video)
-        question = input('¿øÇÏ´Â ¿µ»óÀ» Àç»ıÇÏ½Ã°Ú½À´Ï±î? (y/n)')
-        if question == 'y':
-            button = int(input('Àç»ıÇÏ°íÀÚ ÇÏ´Â ¿µ»óÀÇ ¹øÈ£(Ãâ·ÂµÈ Ç¥ °¡Àå ¿ŞÂÊÀÇ ¹øÈ£)¸¦ ÀÔ·ÂÇØÁÖ¼¼¿ä.'))
-            url = df_just_video['¿µ»óurl'][button]
-            return url.replace("https://www.youtube.com//watch?v=","")
-
+            else:
+                exit()
         else:
             exit()
-    else:
-        exit()
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
 
-    window = YouTubeWindow(get_video())
-    window.show()
 
-    try:
-        sys.exit(app.exec_())
-    except SystemExit:
-        print('player Window Closed')
+    def __del__(self):
+        if self.cap==None:
+            return
+        if self.cap.isOpened():
+            self.cap.release()
+
+    def audioCallback(self,selector, value):
+        self.aux.set_pause(True)  # Stop the audio
+        if selector=="eof":
+            pass
+    pass
+
+pass #End of Class
+
+
+
+if __name__ == "__main__":
+    videoGUI(Tk(), "My Window Title")
+    pass
