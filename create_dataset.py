4bf85717ec54e19b0e13b99cd6887c43fdbadea4
josieon <timekeeperfn@naver.com>
2022-03-30 05:06:08 +0900

    파일 정리

--- create_dataset.py
+++ create_dataset.py
@@ -3,80 +3,71 @@ import mediapipe as mp
 import numpy as np
 import time, os
 
-directory="."
-scaler = 0.5
 actions = ['rewind','advance','stop']
-seq_length=45
+secs_for_action = 10
 
 # MediaPipe hands model
 mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
-created_time=int(time.time())
 hands = mp_hands.Hands(
     max_num_hands=1,
     min_detection_confidence=0.5,
     min_tracking_confidence=0.5)
 
-for idx_dir, subdir in enumerate(actions):
-    action = subdir
-    subdir = directory + "/" + subdir
-    listdir = os.listdir(subdir)
-    print(listdir)
+cap = cv2.VideoCapture(0)
+
+created_time = int(time.time())
+os.makedirs('dataset', exist_ok=True)
+
+while cap.isOpened():
     data = []
-    for idx_mov, mov in enumerate(listdir):
-        vfile = cv2.VideoCapture(subdir + "/" + mov)
-        if vfile.isOpened():
-            while True:
-                vret, img = vfile.read()
-                if vret:
-                    img = cv2.resize(img,(int(img.shape[1] * scaler),int(img.shape[0] * scaler)))
-                    img = cv2.flip(img,1)
-                    img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-                    result = hands.process(img)
-                    img = cv2.cvtColor(img,cv2.COLOR_RGB2BGR)
-                    if result.multi_hand_landmarks is not None:
-                        for res in result.multi_hand_landmarks:
-                            if False:    
-                                print(1)
-                            mp_drawing.draw_landmarks(img, res, mp_hands.HAND_CONNECTIONS)
-                            joint = np.zeros((21,4))
-                            for j, lm in enumerate(res.landmark):
-                                joint[j] = [lm.x, lm.y, lm.z, lm.visibility]
-                            # Compute angles between joints
-                            v1 = joint[[0,1,2,3,0,5,6,7,0,9,10,11,0,13,14,15,0,17,18,19], :3] # Parent joint
-                            v2 = joint[[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20], :3] # Child joint
-                            v = v2 - v1 # [20, 3]
-                            # Normalize v
-                            v = v / np.linalg.norm(v, axis=1)[:, np.newaxis]
+    for idx, action in enumerate(actions):
+        ret, img = cap.read()
+        img = cv2.flip(img, 1)
+        cv2.putText(img, f'Waiting for collecting {action.upper()} action...', org=(10, 30), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(255, 255, 255), thickness=2)
+        cv2.imshow('img', img)
+        cv2.waitKey(1500)
+        start_time = time.time()
+        while time.time() - start_time < secs_for_action:
+            ret, img = cap.read()
 
-                            # Get angle using arcos of dot product
-                            angle = np.arccos(np.einsum('nt,nt->n',
-                                v[[0,1,2,4,5,6,8,9,10,12,13,14,16,17,18],:], 
-                                v[[1,2,3,5,6,7,9,10,11,13,14,15,17,18,19],:])) # [15,]
+            img = cv2.flip(img, 1)
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            result = hands.process(img)
+            img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
 
-                            angle = np.degrees(angle) # Convert radian to degree
+            if result.multi_hand_landmarks is not None:
+                for res in result.multi_hand_landmarks:
+                    joint = np.zeros((21, 4))
+                    for j, lm in enumerate(res.landmark):
+                        joint[j] = [lm.x, lm.y, lm.z, lm.visibility]
 
-                            angle_label = np.array([angle], dtype=np.float32)
-                            angle_label = np.append(angle_label, idx_dir)
+                    # Compute angles between joints
+                    v1 = joint[[0,1,2,3,0,5,6,7,0,9,10,11,0,13,14,15,0,17,18,19], :3] # Parent joint
+                    v2 = joint[[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20], :3] # Child joint
+                    v = v2 - v1 # [20, 3]
+                    # Normalize v
+                    v = v / np.linalg.norm(v, axis=1)[:, np.newaxis]
 
-                            d = np.concatenate([joint.flatten(), angle_label])
+                    # Get angle using arcos of dot product
+                    angle = np.arccos(np.einsum('nt,nt->n',
+                        v[[0,1,2,4,5,6,8,9,10,12,13,14,16,17,18],:], 
+                        v[[1,2,3,5,6,7,9,10,11,13,14,15,17,18,19],:])) # [15,]
 
-                            data.append(d)
-                    #if result.multi_hand_landmarks is not None:
-                        #mp_drawing.draw_landmarks(img, res, mp_hands.HAND_CONNECTIONS)
-                    cv2.imshow('img',img)
-                    cv2.waitKey(1)
-                else:
-                    break
-        else:
-            print("파일을 열 수 없습니다.")
-        vfile.release()
-        cv2.destroyAllWindows()
-    data=np.array(data)
-    np.save(os.path.join('dataset',f'raw_{action}1'),data)
-    full_seq_data=[]
-    for seq in range(len(data)-seq_length):
-        full_seq_data.append(data[seq:seq + seq_length])
-    full_seq_data=np.array(full_seq_data)
-    print(subdir,full_seq_data.shape)
-    np.save(os.path.join('dataset',f'seq_{action}1'),full_seq_data)
+                    angle = np.degrees(angle) # Convert radian to degree
+
+                    angle_label = np.array([angle], dtype=np.float32)
+                    angle_label = np.append(angle_label, idx)
+
+                    data.append(angle_label)
+
+                    mp_drawing.draw_landmarks(img, res, mp_hands.HAND_CONNECTIONS)
+            cv2.resize(img, (1000, 750))
+            cv2.imshow('img', img)
+            if cv2.waitKey(1) == ord('q'):
+                break
+
+    data = np.array(data)
+    with open(f'dataset/data_{created_time}.csv', 'w', encoding='UTF-8', newline='') as file:
+        np.savetxt(file,data, delimiter=',')
+    break
